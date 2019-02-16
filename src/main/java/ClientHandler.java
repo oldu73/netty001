bd1a0c8a8a9002eb39fb04ff179136dd10656256
oldu73 <oldu73@gmail.com>
2019-02-16 23:51:33 +0100

    Initial commit

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,18 @@
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        RequestData msg = new RequestData();
+        msg.setIntValue(18);
+        msg.setStringValue("all work and no play makes jack a dull boy");
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(msg);
+        ctx.close();
+    }
+}
